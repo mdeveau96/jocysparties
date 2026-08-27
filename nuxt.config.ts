67fb7afa24b0b1a2de8e95bb4ba7622902134d19
mdeveau96 <michael_deveau@yahoo.com>
2026-08-26 21:42:44 -0700

    Fixed path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['C:\\Users\\Michael\\jocysparties\\assets\\css\\main.css'],
+  css: ['~/../assets/css/main.css'],
   modules: ['@nuxt/ui']
 })
